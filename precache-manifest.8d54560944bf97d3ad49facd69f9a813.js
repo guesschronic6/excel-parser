93aab6410ef06a759cbe06ed4c94088ce5d9834e
omar tagayan <omaryusuftagayan@gmail.com>
2020-09-21 05:41:41 +0800

    Updates

---
+++ precache-manifest.8d54560944bf97d3ad49facd69f9a813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3232b377b2f54d447a6c0743aa8f7be",
+    "url": "/excel-parser/index.html"
+  },
+  {
+    "revision": "b276a8ddc41de344994b",
+    "url": "/excel-parser/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fcc999506da3253885c5",
+    "url": "/excel-parser/static/js/2.967ee0bb.chunk.js"
+  },
+  {
+    "revision": "fd93936ad05fb41a88e3492e1b7cea4b",
+    "url": "/excel-parser/static/js/2.967ee0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b276a8ddc41de344994b",
+    "url": "/excel-parser/static/js/main.dc1c6846.chunk.js"
+  },
+  {
+    "revision": "23386d3ae3b8c31fc81e",
+    "url": "/excel-parser/static/js/runtime-main.6cf27894.js"
+  }
+]);
